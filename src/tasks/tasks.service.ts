13a0d3d992d1d3bf15e2c878e7b4eb949904e651
lpchi133 <lpchi21@clc.fitus.edu.vn>
2024-12-17 13:57:02 +0700

    fix time

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,26 +12,18 @@ export class TasksService {
     const dateTimeSet = toZonedTime(`${data.start_date}T${data.start_time}`, timeZone);
     const dueDateTime = toZonedTime(`${data.date}T${data.time}`, timeZone);
 
-    // Adjusting for Vietnam timezone (UTC+7)
-    const vietnamOffset = 7 * 60 * 60 * 1000; // 7 hours in milliseconds
-    const localDateTimeSet = new Date(dateTimeSet.getTime() + vietnamOffset);
-    const localDueDateTime = new Date(dueDateTime.getTime() + vietnamOffset);
-
-    console.log('localDateTimeSet:', localDateTimeSet);
-    console.log('localDueDateTime:', localDueDateTime);
-
     return this.prisma.task.create({
-        data: {
-            itemLabel: data.label,
-            itemDescription: data.description,
-            itemPriority: data.priority,
-            itemStatus: data.status,
-            dateTimeSet: localDateTimeSet,
-            dueDateTime: localDueDateTime,
-            userId: userId,
-        },
+      data: {
+        itemLabel: data.label,
+        itemDescription: data.description,
+        itemPriority: data.priority,
+        itemStatus: data.status,
+        dateTimeSet: dateTimeSet,
+        dueDateTime: dueDateTime,
+        userId: userId,
+      },
     });
-}
+  }
 
   async getAllTasks(userId: number) {
     return this.prisma.task.findMany({
@@ -58,12 +50,7 @@ export class TasksService {
     const dateTimeSet = toZonedTime(`${start_date}T${start_time}:00`, timeZone);
     const dueDateTime = toZonedTime(`${date}T${time}:00`, timeZone);
 
-    // Adjusting for Vietnam timezone (UTC+7)
-    const vietnamOffset = 7 * 60 * 60 * 1000; // 7 hours in milliseconds
-    const localDateTimeSet = new Date(dateTimeSet.getTime() + vietnamOffset);
-    const localDueDateTime = new Date(dueDateTime.getTime() + vietnamOffset);
-
-    if (isNaN(localDateTimeSet.getTime()) && isNaN(localDueDateTime.getTime())) {
+    if (isNaN(dueDateTime.getTime()) && isNaN(dateTimeSet.getTime())) {
       throw new Error('Invalid date or time');
     }
 
@@ -84,8 +71,8 @@ export class TasksService {
           itemLabel: label,
           itemDescription: description,
           itemPriority: priority,
-          dateTimeSet: localDateTimeSet,
-          dueDateTime: localDueDateTime,
+          dateTimeSet: dateTimeSet,
+          dueDateTime: dueDateTime,
           itemStatus: status,
         },
       });
