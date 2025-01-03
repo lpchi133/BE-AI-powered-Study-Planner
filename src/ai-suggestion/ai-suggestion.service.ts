fbe0b395fe70eb4d3eee4e5310d4549ab85ffbf2
Hạnh Bảo <102734925+hanh-bao@users.noreply.github.com>
2025-01-03 21:17:47 +0700

    Update AI suggestion

--- src/ai-suggestion/ai-suggestion.service.ts
+++ src/ai-suggestion/ai-suggestion.service.ts
@@ -10,6 +10,19 @@ export class AiSuggestionService {
     private tasksService: TasksService,
   ) {}
 
+  mapTaskToPrompt(task:{    id: number;
+    itemLabel: string;
+    itemDescription: string;
+    itemPriority: string;
+    itemStatus: string;
+    dateTimeSet: string;
+    dueDateTime: string;
+    focusTime: number|null;
+    breakTime:number|null;
+  }) {
+    return `- TaskId:${task.id}, Name: ${task.itemLabel}, Priority: ${task.itemPriority}, Status: ${task.itemStatus}, Start: ${task.dateTimeSet}, Due: ${task.dueDateTime}, Focus Time: ${task.focusTime}, Break Time: ${task.breakTime}`;
+  }
+
   async generateSuggestion(userId: number) {
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
@@ -17,14 +30,22 @@ export class AiSuggestionService {
     const tasks = await this.tasksService.getAllTasks(userId);
 
     const prompt = `
-            Analyze this schedule and provide feedback:
-            ${JSON.stringify(tasks)}
-            Feedback:
+            You are a preofessional in schedule and structuring task. Please analyze this schedule and offering motivational feedback to encourage consistency and improvement:
+            ${tasks.map(this.mapTaskToPrompt).join("\n")}
+            Base on:
             1. Are there any tight schedules?
             2. Suggestions for prioritization.
             3. Recommendations for balance and focus.
+            4. Suggestions for breaks.
+            Notes:
+             - Please reprinting, beautify the tasks list and add any additional information you think is relevant by using markdown no using html (in Notes column).
+             - Please bold the name task when giving suggestions.
+             - Please sort the tasks by priority and due date.
+             - Please add the suggestions at the end of the list.
+             - The suggestions should be note in bullet points with improvements and encouragements.
+             - Make the suggestions as detailed as possible, provide reasons for each suggestion and professional advice.
+             - Please beautify the suggestions by using markdown no using html.
         `;
-
     const result = await model.generateContent(prompt);
 
     return result.response.text();
