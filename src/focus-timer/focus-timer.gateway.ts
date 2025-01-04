a8011fa419c4aa454bddae0f3aa751c2b8d529ed
lpchi133 <lpchi21@clc.fitus.edu.vn>
2025-01-05 01:24:41 +0700

    websocket - End timer and notify user if task completes early + update status task

--- src/focus-timer/focus-timer.gateway.ts
+++ src/focus-timer/focus-timer.gateway.ts
@@ -14,12 +14,14 @@ import { Server, Socket } from "socket.io";
 export class FocusTimerGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer() server: Server;
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     handleConnection(client: Socket) {
-        console.log(`Client connected: ${client.id}`);
+        // console.log(`Client connected: ${client.id}`);
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     handleDisconnect(client: Socket) {
-        console.log(`Client disconnected: ${client.id}`);
+        // console.log(`Client disconnected: ${client.id}`);
     }
 
     notifyUser(taskId: number, message: string) {
